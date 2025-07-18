b93648b528181d12bee9033ba6270a3c0da0c880
samueljimenezdev <sajimenezr1@gmail.com>
2025-07-18 15:42:25 -0500

    Create repository, and initial structure of Product  entity

--- src/main/java/com/samueljimenez/productsmanager/model/Producto.java
+++ src/main/java/com/samueljimenez/productsmanager/model/Producto.java
@@ -1,4 +1,53 @@
 package com.samueljimenez.productsmanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private BigDecimal precio;
+
+    private Integer stock;
+
+    public Producto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
 }
---
+++ src/main/java/com/samueljimenez/productsmanager/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.samueljimenez.productsmanager.repository;
+
+import com.samueljimenez.productsmanager.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
