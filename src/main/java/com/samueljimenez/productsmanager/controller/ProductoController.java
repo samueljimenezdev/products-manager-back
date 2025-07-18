7a1c50c041bd269c598ce566ae3a9a4081013aab
samueljimenezdev <sajimenezr1@gmail.com>
2025-07-18 15:31:43 -0500

    Create folder structure, and controller and entity class

---
+++ src/main/java/com/samueljimenez/productsmanager/controller/ProductoController.java
@@ -0,0 +1,8 @@
+package com.samueljimenez.productsmanager.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductoController {
+
+}
---
+++ src/main/java/com/samueljimenez/productsmanager/model/Producto.java
@@ -0,0 +1,4 @@
+package com.samueljimenez.productsmanager.model;
+
+public class Producto {
+}
