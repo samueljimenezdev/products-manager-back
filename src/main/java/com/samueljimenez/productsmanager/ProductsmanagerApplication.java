99b2c929a2e6cf60793672d7b435239d6657b934
samueljimenezdev <sajimenezr1@gmail.com>
2025-07-18 15:15:55 -0500

    First commit

---
+++ src/main/java/com/samueljimenez/productsmanager/ProductsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.samueljimenez.productsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samueljimenez/productsmanager/ProductsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueljimenez.productsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
